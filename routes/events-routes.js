81341ec96148dc57d801b4ed0a3fee1078c4c1e4
manavsharmah <manavsarmadk@gmail.com>
2024-08-01 18:18:42 +0530

    Caching of common datas

--- routes/events-routes.js
+++ routes/events-routes.js
@@ -2,14 +2,20 @@ const express = require("express");
 const { check } = require("express-validator");
 const router = express.Router();
 const { verifyToken, checkAdmin } = require('../middleware/verify-token');
-const { addEvents, getEvents } = require('../controllers/events-controller.js')
+const { addEvents, getEvents } = require('../controllers/events-controller.js');
+
+// Middleware to set Cache-Control header
+const setCacheControl = (req, res, next) => {
+  res.set('Cache-Control', 'public, max-age=3600'); // Cache for 1 hour
+  next();
+};
 
 router.post("/upload", [
     check("title").not().isEmpty(),
     check("content").not().isEmpty(),
     check("event_date").isISO8601(),
-], verifyToken, checkAdmin, addEvents );
+], verifyToken, checkAdmin, addEvents);
 
-router.get("/get-events", getEvents);
+router.get("/get-events", setCacheControl, getEvents);
 
 module.exports = router;
--- routes/news-routes.js
+++ routes/news-routes.js
@@ -2,16 +2,21 @@ const express = require("express");
 const { check } = require("express-validator");
 const router = express.Router();
 const { verifyToken, checkAdmin } = require('../middleware/verify-token');
-const {getNews, uploadNews, getSingleNews } = require('../controllers/news-controller');
+const { getNews, uploadNews, getSingleNews } = require('../controllers/news-controller');
+
+// Middleware to set Cache-Control header
+const setCacheControl = (req, res, next) => {
+  res.set('Cache-Control', 'public, max-age=3600'); // Cache for 1 hour
+  next();
+};
 
 router.post("/upload", [
     check("title").not().isEmpty(),
     check("content").not().isEmpty()
 ], verifyToken, checkAdmin, uploadNews);
 
-router.get("/get-news", getNews);
+router.get("/get-news", setCacheControl, getNews);
 
-router.get("/get-news/:id", getSingleNews);
-
+router.get("/get-news/:id", setCacheControl, getSingleNews);
 
 module.exports = router;
