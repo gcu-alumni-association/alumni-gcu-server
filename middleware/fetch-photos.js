4a7a48ad876541d0c8bf7fb18f4464f9180e24bd
PriyanujBora <priybora01@outlook.com>
2024-08-26 14:32:15 +0530

    Commented out the whole code as it is not necessary, will delete in future.

--- middleware/fetch-photos.js
+++ middleware/fetch-photos.js
@@ -1,19 +1,19 @@
-const Photos = require('../model/Photos'); 
+// const Photos = require('../model/Photos'); 
 
-const fetchPhotos = async (req, res, next) => {
-  try {
-    //Fetch all photo documents from MongoDB
-    const photos = await Photos.find({});
+// const fetchPhotos = async (req, res, next) => {
+//   try {
+//     //Fetch all photo documents from MongoDB
+//     const photos = await Photos.find({});
 
-    //Extract only the URLs from the photos documents
-    const photoUrls = photos.map(photo => photo.url);
+//     //Extract only the URLs from the photos documents
+//     const photoUrls = photos.map(photo => photo.url);
 
-    req.photoUrls = photoUrls; //Store photo URLs in the request object
-    next();
-  } catch (err) {
-    console.error('Error fetching photo URLs:', err);
-    res.status(500).json({ message: 'An error occurred while fetching photo URLs' });
-  }
-};
+//     req.photoUrls = photoUrls; //Store photo URLs in the request object
+//     next();
+//   } catch (err) {
+//     console.error('Error fetching photo URLs:', err);
+//     res.status(500).json({ message: 'An error occurred while fetching photo URLs' });
+//   }
+// };
 
-module.exports = fetchPhotos;
+// module.exports = fetchPhotos;
