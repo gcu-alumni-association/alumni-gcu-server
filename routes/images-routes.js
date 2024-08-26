4f1daec79e98c4c0d1e2603af8c69cd548f8266b
PriyanujBora <priybora01@outlook.com>
2024-08-26 14:45:51 +0530

    Changed the previous logic of fetching images. Also added code to upload the image for gallery from here itself instead of uploading through admin routes.

--- routes/images-routes.js
+++ routes/images-routes.js
@@ -1,22 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Photos = require('../model/Photos');
+const Photos = require("../model/Photos");
+const { verifyToken, checkAdmin } = require("../middleware/verify-token");
+const { upload, uploadImage } = require("../middleware/upload-images");
+const { uploadImageForGallery, getImagesForGallery } = require("../controllers/gallery-controller");
 
 // Middleware to set Cache-Control header
-const setCacheControl = (req, res, next) => {
-  res.set('Cache-Control', 'public, max-age=3600'); // Cache for 1 hour
-  next();
-};
+// const setCacheControl = (req, res, next) => {
+// 	res.set("Cache-Control", "public, max-age=3600"); // Cache for 1 hour
+// 	next();
+// };
 
-router.get('/photos', setCacheControl, async (req, res) => {
-  try {
-    const photos = await Photos.find({});
-    const photoUrls = photos.map(photo => photo.url);
-    res.status(200).json(photoUrls);
-  } catch (err) {
-    console.error('Error fetching photos:', err);
-    res.status(500).json({ message: 'An error occurred while fetching photos' });
-  }
-});
+router.get(
+	"/get-photos",
+	// setCacheControl,
+	getImagesForGallery
+);
+
+router.post(
+	"/upload",
+	upload.single("image"),
+	verifyToken,
+	checkAdmin,
+	uploadImage,
+	uploadImageForGallery
+);
 
 module.exports = router;
