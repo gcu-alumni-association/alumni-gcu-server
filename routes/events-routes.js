d612190e69fb202772f90dfebdcf60db55ea6743
PriyanujBora <priybora01@outlook.com>
2024-09-27 20:17:21 +0530

    changed to be able to send multiple images, 5 in this case, as array

--- routes/events-routes.js
+++ routes/events-routes.js
@@ -5,27 +5,21 @@ const { verifyToken, checkAdmin } = require("../middleware/verify-token");
 const { addEvents, getEvents, getSingleEvent } = require("../controllers/events-controller.js");
 const { uploadImage, upload } = require("../middleware/upload-images");
 
-// Middleware to set Cache-Control header
-const setCacheControl = (req, res, next) => {
-	res.set("Cache-Control", "public, max-age=3600"); // Cache for 1 hour
-	next();
-};
-
 router.post(
-	"/upload",
-	upload.single("image"),
-	[
-		check("title").not().isEmpty(),
-		check("content").not().isEmpty(),
-		check("event_date").isISO8601(),
-	],
-	verifyToken,
-	checkAdmin,
-  uploadImage, 
-	addEvents
+  "/upload",
+  verifyToken,
+  checkAdmin,
+  upload.array("images", 5),
+  uploadImage,
+  [
+    check("title").not().isEmpty(),
+    check("content").not().isEmpty(),
+    check("event_date").isISO8601(),
+  ],
+  addEvents
 );
 
-router.get("/get-events", setCacheControl, getEvents);
+router.get("/get-events", getEvents);
 
 router.get("/get-event/:id", getSingleEvent);
 
