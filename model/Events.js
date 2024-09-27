23cd4fa4864223a856ff6b43a5b30e1577369b95
PriyanujBora <priybora01@outlook.com>
2024-09-27 20:15:00 +0530

    Changed the model to store the imagefolder (subfolder) name as well as the images which are sent to the database as array

--- model/Events.js
+++ model/Events.js
@@ -5,36 +5,32 @@ const eventsSchema = new Schema({
     title: {
         type: String,
         required: true
-
     },
-
     content: {
         type: String,
         required: true
     },
-
     organizer: {
         type: String,        
     },
-
     event_date: {
         type: Date,
         required: true
     },
-
     event_time: {
         type: String,
     },
-
     posted_date: {
         type: Date,
         default: Date.now
-    }, 
-
-    imageUrl : {
-        type: String,
+    },
+    images: {
+        type: [String],  // Array of strings to store image paths
+        default: []
+    },
+    imageFolder: {
+        type: String  // To store the name of the folder containing the images
     }
-    
 });
 
 module.exports = mongoose.model('Events', eventsSchema);
